381c36d34d52c200c48797a6ad1415d2d7028b39
jaygala24 <jaygala24@gmail.com>
2018-11-29 09:38:56 +0530

    Updates

---
+++ precache-manifest.3c974f0b19a12b160de93e5a88f35648.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "92700b96a6acba4945f6",
+    "url": "/profile/static/js/runtime~main.92700b96.js"
+  },
+  {
+    "revision": "aa50c6cb498707bd608b",
+    "url": "/profile/static/js/main.aa50c6cb.chunk.js"
+  },
+  {
+    "revision": "7f9a50cd80cedb92c5f5",
+    "url": "/profile/static/js/1.7f9a50cd.chunk.js"
+  },
+  {
+    "revision": "aa50c6cb498707bd608b",
+    "url": "/profile/static/css/main.2fe77b51.chunk.css"
+  },
+  {
+    "revision": "7f9a50cd80cedb92c5f5",
+    "url": "/profile/static/css/1.608b9a24.chunk.css"
+  },
+  {
+    "revision": "69e6ea20c4850431c92c95db99ebc2ef",
+    "url": "/profile/index.html"
+  }
+];
